fe38041da301c9991170c482dd9484d6337a32d1
Вадим <vadim.antipov.2005@yandex.ru>
2025-06-16 20:37:50 +0300

    Присоединение новых роутеров

---
+++ handlers/users/admin/__init__.py
@@ -0,0 +1,15 @@
+from aiogram import Router
+
+from handlers.users.admin.notify_all_users import router as notify_all_users_router
+from handlers.users.admin.notify_one_user import router as notify_one_user_router
+from handlers.users.admin.admin import router as admin_router
+
+__all__ = ['router']
+
+router = Router(name=__name__)
+
+router.include_routers(
+    admin_router,
+    notify_all_users_router,
+    notify_one_user_router,
+)
