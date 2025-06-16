ea4fa003f5f3ed12455da96768cb19ca71d041e5
Вадим <vadim.antipov.2005@yandex.ru>
2025-06-16 20:41:18 +0300

    Добавлен фильтр для проверки на админа

---
+++ filters/admin.py
@@ -0,0 +1,8 @@
+from aiogram.filters import Filter
+from aiogram.types import Message
+
+from settings import settings
+
+class AdminFilter(Filter):
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in settings.admins
