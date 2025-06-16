e356ac7550d69e9b1faed18055c127b1cf9e445d
Вадим <vadim.antipov.2005@yandex.ru>
2025-06-16 20:41:36 +0300

    Обработка команды /admin

---
+++ handlers/users/admin/admin.py
@@ -0,0 +1,19 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+
+from sqlalchemy.orm import sessionmaker
+
+
+from filters.admin import AdminFilter
+
+router = Router(name=__name__)
+
+@router.message(AdminFilter(), Command('admin'))
+async def admin_command(msg: Message, state: FSMContext, session: sessionmaker):
+    message = ("Список всех команд, которые доступны:\n\n"
+               "Оповещения:\n"
+               "/notify_user - уведомить одного пользователя; Требуется user_id пользователя и отправляемое сообщение\n"
+               "/notify_all_users - уведомить всех пользователей; Требуется отправляемое сообщение\n")
+    await msg.answer(message)
