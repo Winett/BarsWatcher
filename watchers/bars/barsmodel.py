bb489f62a464336b970416562da79929f811406c
Вадим <vadim.antipov.2005@yandex.ru>
2025-06-30 09:46:03 +0300

    Изменение текста при изменении оценок

--- watchers/bars/barsmodel.py
+++ watchers/bars/barsmodel.py
@@ -14,15 +14,15 @@ class DisciplineWatcher(BaseModel):
 
         for i, (old_mark, new_mark) in enumerate(zip(self.marks, other.marks)):
             if old_mark != new_mark:
-                changes.append(f"КМ-{i + 1}: {old_mark} → {new_mark}")
+                changes.append(f"Изменение в {self.discipline}: КМ-{i + 1}: {old_mark} → {new_mark}")
 
         if self.mark_PA != other.mark_PA:
-            changes.append(f"ПА: {self.mark_PA} → {other.mark_PA}")
+            changes.append(f"Изменение в {self.discipline}: ПА: {self.mark_PA} → {other.mark_PA}")
 
         if self.mark_final != other.mark_final:
-            changes.append(f"Итоговая оценка: {self.mark_final} → {other.mark_final}")
+            changes.append(f"Изменение в {self.discipline}: Итоговая оценка: {self.mark_final} → {other.mark_final}")
 
         if len(self.marks) != len(other.marks):
-            changes.append(f"Количество КМ изменилось: {len(self.marks)} → {len(other.marks)}")
+            changes.append(f"Изменение в {self.discipline}: Количество КМ изменилось: {len(self.marks)} → {len(other.marks)}")
 
         return changes
