72fd27f30befb28f2048bda29bd53840dfedf334
Вадим <vadim.antipov.2005@yandex.ru>
2026-07-06 13:09:55 +0300

    Исправлена отсылка сообщений на обращения от пользователей /suggestion и /report

--- handlers/users/command/report.py
+++ handlers/users/command/report.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -6,28 +8,40 @@ from aiogram.fsm.context import FSMContext
 from sqlalchemy.orm import sessionmaker
 
 from services.notification import NotificationService
+from settings import settings
 from states.reportState import ReportState
 
 router = Router(name=__name__)
 
 
 @router.message(Command('report'))
-async def suggestion_command(msg: Message, state: FSMContext, session: sessionmaker):
+async def report_command(msg: Message, state: FSMContext, session: sessionmaker):
     await state.set_state(ReportState.message_wait)
     await msg.answer(
         'Введите ваш репорт:\n\n*Если передумали отправлять сообщение или что-то ещё, просто введите /start, чтобы не отправить выйти из режима отправки предложений')
 
 
 @router.message(ReportState.message_wait)
-async def suggestion_command(msg: Message, state: FSMContext, session: sessionmaker):
-    if msg.text == '/start':
+async def report_wait(msg: Message, state: FSMContext, session: sessionmaker):
+    if msg.text and msg.text == '/start':
         await state.clear()
         return
-    message = (f'ОТПРАВЛЕН РЕПОРТ ПО БОТУ\n'
-               f'От: @{msg.from_user.username} <code>{msg.from_user.id}</code> {msg.from_user.first_name} {msg.from_user.last_name}\n'
-               f'\n{msg.text}')
+
+    header = (f'ОТПРАВЛЕН РЕПОРТ ПО БОТУ\n'
+              f'От: @{msg.from_user.username} <code>{msg.from_user.id}</code> '
+              f'{msg.from_user.first_name} {msg.from_user.last_name}')
+
     notification_service = NotificationService(session)
-    await notification_service.notify_admins(message=message)
+    for admin_id in settings.admins:
+        sent = await notification_service.bot.send_message(admin_id, header)
+        await notification_service.bot.copy_message(
+            chat_id=admin_id,
+            from_chat_id=msg.chat.id,
+            message_id=msg.message_id,
+            reply_to_message_id=sent.message_id,
+        )
+        await asyncio.sleep(0.2)
+
     await state.clear()
     await msg.answer('Спасибо!\n'
                      'Ваш репорт был отправлен!')
--- handlers/users/command/suggestion.py
+++ handlers/users/command/suggestion.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -6,6 +8,7 @@ from aiogram.fsm.context import FSMContext
 from sqlalchemy.orm import sessionmaker
 
 from services.notification import NotificationService
+from settings import settings
 from states.suggestionState import SuggestionState
 
 router = Router(name=__name__)
@@ -16,15 +19,26 @@ async def suggestion_command(msg: Message, state: FSMContext, session: sessionma
     await msg.answer('Введите ваше предложение:\n\n*Если передумали отправлять сообщение или что-то ещё, просто введите /start, чтобы не отправить выйти из режима отправки предложений')
 
 @router.message(SuggestionState.message_wait)
-async def suggestion_command(msg: Message, state: FSMContext, session: sessionmaker):
-    if msg.text == '/start':
+async def suggestion_wait(msg: Message, state: FSMContext, session: sessionmaker):
+    if msg.text and msg.text == '/start':
         await state.clear()
         return
-    message = (f'ОТПРАВЛЕНО ПРЕДЛОЖЕНИЕ ПО УЛУЧШЕНИЮ БОТА\n'
-               f'От: @{msg.from_user.username} <code>{msg.from_user.id}</code> {msg.from_user.first_name} {msg.from_user.last_name}\n'
-               f'\n{msg.text}')
+
+    header = (f'ОТПРАВЛЕНО ПРЕДЛОЖЕНИЕ ПО УЛУЧШЕНИЮ БОТА\n'
+              f'От: @{msg.from_user.username} <code>{msg.from_user.id}</code> '
+              f'{msg.from_user.first_name} {msg.from_user.last_name}')
+
     notification_service = NotificationService(session)
-    await notification_service.notify_admins(message=message)
+    for admin_id in settings.admins:
+        sent = await notification_service.bot.send_message(admin_id, header)
+        await notification_service.bot.copy_message(
+            chat_id=admin_id,
+            from_chat_id=msg.chat.id,
+            message_id=msg.message_id,
+            reply_to_message_id=sent.message_id,
+        )
+        await asyncio.sleep(0.2)
+
     await state.clear()
     await msg.answer('Спасибо!\n'
                      'Ваше предложение было отправлено!')
